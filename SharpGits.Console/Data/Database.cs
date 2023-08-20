668dd122c07f5460e8eac3c4caf13f7abeedee41
Amanda Prospal <aprospal@litmus.com>
2023-08-20 16:49:11 -0400

    Implement creation of init directories

--- SharpGits.Console/Data/Database.cs
+++ SharpGits.Console/Data/Database.cs
@@ -4,7 +4,20 @@ public class Database
 {
     public static void Init(string workspacePath)
     {
-        // TODO Create git database directory and appropriate subdirs.
-        throw new NotImplementedException();
+	    var gitDirPath = Path.Combine(workspacePath, ".git");
+
+	    if (Directory.Exists(gitDirPath))
+	    {
+		    return;
+	    }
+
+		CreateInitDirs(gitDirPath);
     }
+
+    private static void CreateInitDirs(string gitDirPath)
+    {
+	    Directory.CreateDirectory(gitDirPath);
+		Directory.CreateDirectory(Path.Combine(gitDirPath, "objects"));
+	    Directory.CreateDirectory(Path.Combine(gitDirPath, "refs"));
+	}
 }
