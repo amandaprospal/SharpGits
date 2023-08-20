0a064fcb05963379bbd12acab24179cfd290b70e
Amanda Prospal <aprospal@litmus.com>
2023-08-20 17:04:42 -0400

    Implement creation of HEAD file on git init

--- SharpGits.Console/Data/Database.cs
+++ SharpGits.Console/Data/Database.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace SharpGits.Console.Data;
 
 public class Database
@@ -12,6 +14,7 @@ public class Database
 	    }
 
 		CreateInitDirs(gitDirPath);
+		CreateHeadFile(gitDirPath);
     }
 
     private static void CreateInitDirs(string gitDirPath)
@@ -20,4 +23,12 @@ public class Database
 		Directory.CreateDirectory(Path.Combine(gitDirPath, "objects"));
 	    Directory.CreateDirectory(Path.Combine(gitDirPath, "refs"));
 	}
+
+    private static void CreateHeadFile(string gitDirPath)
+    {
+	    var filePath = Path.Combine(gitDirPath, "HEAD");
+	    using var fs = File.Create(filePath);
+	    var fileContents = new UTF8Encoding(true).GetBytes("ref: refs/heads/main\n");
+	    fs.Write(fileContents, 0, fileContents.Length);
+    }
 }
